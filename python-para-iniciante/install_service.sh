de91f1d889eee02423fbfd92876f6cafaefcb046
schinaid <andersonschinaid@hotmail.com>
2025-08-09 18:39:22 +0000

    ajustando script de iniciação

--- python-para-iniciante/install_service.sh
+++ python-para-iniciante/install_service.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-sudo apt install net-tools
 sudo apt-get install -y curl
 curl -fsSL https://ollama.com/install.sh | sh
 ollama serve &
+ollama pull llama3.2:3b-instruct-q4_K_M
 
 ollama list
 
 ollama run llama3
-ollama pull llama3.2:3b-instruct-q4_K_M
