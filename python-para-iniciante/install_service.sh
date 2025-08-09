dcd95512ed3cd42a9742bf6b1f7392fa54e4006f
schinaid <andersonschinaid@hotmail.com>
2025-08-09 18:14:46 +0000

    refatoramento do código

--- python-para-iniciante/install_service.sh
+++ python-para-iniciante/install_service.sh
@@ -3,6 +3,8 @@ sudo apt install net-tools
 sudo apt-get install -y curl
 curl -fsSL https://ollama.com/install.sh | sh
 ollama serve &
+
 ollama list
 
 ollama run llama3
+ollama pull llama3.2:3b-instruct-q4_K_M
